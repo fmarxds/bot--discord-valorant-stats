4f9e96bbc8e4f3c86827b49e24f2c4241396dd57
Felipe Marques <fmarques.ds@gmail.com>
2022-02-16 23:44:22 -0300

    Ajustes do template

--- main.py
+++ main.py
@@ -4,50 +4,45 @@ import discord
 import requests
 import json
 
-
 client = discord.Client()
 
 
 def is_message_valid(message):
-    
     if message.author == client.user:
         return False
-    
+
     if len(message.content) <= 6:
         return False
-   
+
     if not message.content.startswith('!vava '):
         return False
 
     return True
 
 
-async def get_player_data(message, playerNameTag):
+async def get_player_data(message, player_name_tag):
+    player_info = player_name_tag.split('#')
 
-    playerInfo = playerNameTag.split('#')
-    
-    response = requests.get(f'https://api.henrikdev.xyz/valorant/v2/mmr/na/{playerInfo[0]}/{playerInfo[1]}')
+    response = requests.get(f'https://api.henrikdev.xyz/valorant/v2/mmr/na/{player_info[0]}/{player_info[1]}')
     response_json = json.loads(response.text)
 
     competitive_data = response_json['data']['current_data']
     rank = competitive_data['currenttierpatched']
     level = competitive_data['ranking_in_tier']
 
-    await message.channel.send(f"{playerInfo[0]}#{playerInfo[1]}\nRANK: {rank} ({level}/100)")
-    
+    await message.channel.send(f"{player_info[0]}#{player_info[1]}\nRANK: {rank} ({level}/100)")
+
 
 @client.event
 async def on_ready():
-    
     print('Login successful. USER: {0.user}'.format(client))
 
 
 @client.event
 async def on_message(message):
-    
     if not (is_message_valid(message)):
         return
-    
+
     args = message.content.split(' ', 2)
     command = args[1]
     parameter = args[2]
@@ -56,7 +51,6 @@ async def on_message(message):
         await message.channel.send(f"Comando {command} nao cadastrado.")
 
     await get_player_data(message, parameter)
-    
 
 
 keep_alive()
